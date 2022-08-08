671ea3a4cda5975eabc78bd97728ca06ea506153
den1aka1aki <lavrov.den@gmail.com>
2022-08-08 21:47:09 +0200

    finished_new_react_lesson

--- src/app/components/post.jsx
+++ src/app/components/post.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import {useHistory} from "react-router-dom";
 
 const Post = ({id, posts}) => {
-
+    const history = useHistory()
     const getPostById = (id) =>{
         return posts.find((post) => post.id.toString() === id)
     }
+    const handleSave= () => {
+        history.replace('/posts')
+    }
     const post = getPostById(id)
-    return (
+    return (<>
         <h2>{post ? post.label:`Post with this id:${id} not found`}</h2>
+        <button onClick={()=>{handleSave()}}>Сохранить</button>
+        </>
     );
 };
 
