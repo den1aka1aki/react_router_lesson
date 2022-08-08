2a6fe771977f08530a1ee68daf4e16c6520f3df7
den1aka1aki <lavrov.den@gmail.com>
2022-08-08 12:07:53 +0200

    starting_new_react_lesson

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,28 @@
 import NavBar from "./components/navBar";
+import {Route, Switch, Redirect} from 'react-router-dom';
+import Dashboard from "./components/dashboard";
+import Login from "./components/login";
+import Home from "./components/home";
+import Posts from "./components/posts";
+import NotFound from "./components/not-found";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
     <h1>App</h1>
+        <Switch>
+          <Route path='/dashboard' component={Dashboard}/>
+          <Route path='/posts/:postId?' component={Posts}/>
+          <Route path='/login' component={Login}/>
+          <Route path='/' exact component={Home}/>
+          <Redirect from='/admin' to='/dashboard'/>
+          <Route path='/404' component={NotFound}/>
+          <Redirect to='/404'/>
+
+        </Switch>
     </div>
+
   );
 }
 
--- src/app/components/navBar.jsx
+++ src/app/components/navBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import {Link} from "react-router-dom";
 const NavBar = () => {
     return (
         <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/login">Login</a></li>
-                <li><a href="/posts">Posts</a></li>
-                <li><a href="/dashboard">Dashboard</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/posts">Posts</Link></li>
+                <li><Link to="/dashboard">Dashboard</Link></li>
         </ul>
     );
 };
